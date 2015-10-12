043a6023ec71467a8332ce96b3657edda031838f
sbuzoianu <stefan@buzoianu.ro>
2015-10-12 20:12:54 +0300

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Stefan on 12/10/15.
+//  Copyright © 2015 Stefan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
